9b705493afd80f61d50df140a761a14cd1a96995
Paarshva Chitaliya <paarshva111@gmail.com>
2023-11-20 23:50:03 +0530

    added app

---
+++ android/app/src/main/kotlin/com/example/tiaa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tiaa
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:tiaa/language/changeLanguageDropdown.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:tiaa/l10n/l10n.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'Models/Utils.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  static void setLocale(BuildContext context, Locale newLocale) async {
+    _MainAppState? state = context.findAncestorStateOfType<_MainAppState>();
+
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setString('languageCode', newLocale.languageCode);
+    if (kDebugMode) {
+      print(newLocale.languageCode);
+    }
+    state?.setState(() {
+      state._locale = newLocale;
+    });
+  }
+
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Locale _locale = const Locale('en');
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchLocale().then((locale) {
+      setState(() {
+        _locale = locale;
+      });
+    });
+  }
+
+  Future<Locale> _fetchLocale() async {
+    var prefs = await SharedPreferences.getInstance();
+
+    String languageCode = prefs.getString('languageCode') ?? 'en';
+    if (kDebugMode) {
+      print(languageCode);
+    }
+    return Locale(languageCode);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print(_locale.countryCode);
+    }
+    return AdaptiveTheme(
+        light: ThemeData(brightness: Brightness.light),
+        dark: ThemeData(brightness: Brightness.dark),
+        initial: AdaptiveThemeMode.light,
+        builder: (theme, darkTheme) {
+          return MaterialApp(
+            locale: _locale,
+            debugShowCheckedModeBanner: false,
+            supportedLocales: L10n.all,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              FallbackCupertinoLocalisationsDelegate(),
+            ],
+            theme: theme,
+            darkTheme: darkTheme,
+            scaffoldMessengerKey: messengerKey,
+            home: const SelectLanguageDropdown(),
+          );
+        });
+  }
+}
+
+class FallbackCupertinoLocalisationsDelegate
+    extends LocalizationsDelegate<CupertinoLocalizations> {
+  const FallbackCupertinoLocalisationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => true;
+
+  @override
+  Future<CupertinoLocalizations> load(Locale locale) =>
+      DefaultCupertinoLocalizations.load(locale);
+
+  @override
+  bool shouldReload(FallbackCupertinoLocalisationsDelegate old) => false;
+}
