5e74914b5151d1f728ce65a33bba6b1631d30047
Aman <aman2003nambisan@gmail.com>
2024-06-12 18:18:14 +0530

    feat: added cronjob for supabase

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,7 +3,7 @@ import { users } from "@/lib/db/schema/roleBased";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  await db.select().from(users)
+  await db.select().from(users);
   return NextResponse.json({
     name: "Cron job",
   });
